19bc6012eb9361f995d89320b7bf284569beba6f
Hichamchaguer <chaguer4@gmail.com>
2026-05-06 02:23:15 +0100

    auth done

--- backend/src/database/routes/user_auth.ts
+++ backend/src/database/routes/user_auth.ts
@@ -1,10 +1,11 @@
 import router from 'express';
 import User from '../models/user';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const routes = router.Router();
 
-routes.post('/register', async (req, res) => {
+routes.post('/register', async (req: any, res: any) => {
 
     try {
 
@@ -18,12 +19,81 @@ routes.post('/register', async (req, res) => {
         email: req.body.email,
         password: hashedPassword,
     });
-    res.json(user);
+
+        res.send(await user.save());
+        console.log(User)
     }
     catch (err) {
         console.error('Error creating user:', err);
         res.status(500).json({ error: 'Error creating user' });
     }
+});
+
+// login via jwt 
+
+routes.post('/login', async (req: any, res: any) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        console.log('>>>>> User:', user);
+        if (!user) {
+            console.log('User not found:', req.body.username);
+            return res.status(404).send({ error: 'Invalid username or password 1' });
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            console.log('Invalid password for user:', req.body.username);
+            return res.status(400).send({ error: 'Invalid username or password 2' });
+        }
+
+        const token = jwt.sign({ id: user._id }, 'secret');
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+
+        })
+        res.send({ message: 'Login successful'});
+    }
+    catch (err) {
+        res.status(401).json({ error: 'Unauthenticated' });
+    }
+});
+
+routes.get('/user', async (req:any, res:any) => {
+    try {
+        const token = req.cookies.jwt;
+        if (!token) {
+            return res.send({ user: 'Unauthenticated1' });
+        }
+        const decoded: any = jwt.verify(token, 'secret');
+        const user = await User.findOne({ _id: decoded.id });
+        console.log('>>>>> User:', user);
+        if (!user) {
+            return res.send({ user: 'Unauthenticated2' });
+        }
+        res.send(user);
+    }
+    catch (err) {
+        console.error('Error fetching user:', err);
+        res.status(404).json({ error: 'Error fetching user' });
+    }
+});
+
+routes.post('/logout', (req: any, res: any) => {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.send({ message: 'Logout successful' });
+});
+
+routes.get('/profile', async (req: any, res: any) => {
+
+        try {
+                const users = await User.find();
+                res.send({ message: users.map((u: any) => u.username) });
+        }
+        catch (err) {
+            console.error('Error fetching users:', err);
+            res.status(500).json({ error: 'Error fetching users' });
+        }
 });
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import routes from './database/routes/user_auth';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 
 mongoose.connect('mongodb://localhost:27017/hypertube_server').then(() => {
@@ -10,6 +12,11 @@ mongoose.connect('mongodb://localhost:27017/hypertube_server').then(() => {
 });
 
 const app = express();
+app.use(cookieParser());
+app.use(cors({
+  credentials: true,
+  origin: ['*']
+}));
 const PORT = 3002;
 app.use(express.json()); 
 app.use('/api', routes);
